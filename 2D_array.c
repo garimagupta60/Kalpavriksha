7e0c6e758691b3486278d9c0a6f85ca7ba60315b
garimagupta60 <99130046+garimagupta60@users.noreply.github.com>
2025-01-08 14:47:08 +0530

    Update 2D_array.c

--- 2D_array.c
+++ 2D_array.c
@@ -13,50 +13,50 @@ char to_lower_case(char ch) {
     }
     return ch;
 }
+int is_vowel(char ch){
+    if(ch=='a'||ch=='e' || ch=='i'|| ch=='o'||ch=='u'){
+        return 1;
+    }
+    return 0;
+}
 int main(){
     int rows, cols;
-    
+    int maxlength=0;
     printf("Enter number of rows: ");
     scanf("%d", &rows);
     printf("Enter number of columns: ");
     scanf("%d", &cols);
-    
+
+    if(rows<1 || rows>10 || cols<1 || cols>101){
+        printf("Enter valid row b/w 1 to 10 and valid column b/w 1 to 101\n");
+    }
     while(getchar()!='\n');
     
-    char student_name[10][3][50];
-    
+    char student_name[100][200][50];
+    int names_start_vowel = 0;
     printf("Enter the names:\n");
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             printf("Name at (%d,%d): ", i, j);
-            fgets(student_name[i][j], 50, stdin);
-            student_name[i][j][strcspn(student_name[i][j], "\n")] = '\0';
-        }
-    }
-    
-    printf("\nThe 2D array of names is:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%s ", student_name[i][j]);
-        }
-        printf("\n");
-    }
-    
-    int names_start_vowel = 0;
-    char longest_name[80]="";
-    
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            char first_char = to_lower_case(student_name[i][j][0]); 
-            if (first_char == 'a' || first_char == 'e' || first_char == 'i' || first_char == 'o' || first_char == 'u') {
+            scanf("%s",student_name[i][j]);
+            getchar();
+            if(len(student_name[i][j])>maxlength){
+                maxlength = len(student_name[i][j]);
+            }
+            if(is_vowel(to_lower_case(student_name[i][j][0]))){
                 names_start_vowel++;
             }
-            if (len(student_name[i][j]) > len(longest_name)) {
-                strcpy(longest_name, student_name[i][j]);
+        }
+    }
+    printf("\nNumber of names starting with a vowel: %d\n", names_start_vowel);
+    printf("\nThe longest name:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if(is_vowel(to_lower_case(student_name[i][j][0])){ 
+                if (len(student_name[i][j])==maxlength)
+                    printf("%s\n",student_name[i][j]);
             }
         }
     }
-    
-    printf("\nNumber of names starting with a vowel: %d\n", names_start_vowel);
-    printf("The longest name: %s\n", longest_name);
+    return 0;
 }
